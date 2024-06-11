1827f5fbbeba87eba0728a15fb8d95316e1bbcbf
FionaMaci <117476995+FionaMaci@users.noreply.github.com>
2024-06-12 01:31:51 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const path = require('path');
+const recipeRoutes = require('./routes/recipes');
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+console.log('MongoDB URI:', process.env.MONGODB_URI);
+
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB', err);
+  });
+
+app.use('/api', recipeRoutes);
+
+// Serve the index.html file for the root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Server running on port ${process.env.PORT}');
+});
